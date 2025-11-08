dad1fc1b1427e31083a975d888cfd862c451f191
deepakpradhan624 <pradhand92041@gmail.com>
2025-11-08 12:05:52 +0530

    server setup done

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,18 @@
-function login(username: string): boolean | undefined {
-    if (username === 'admin') {
-        return true;
-    }
-}
+import app from './app.ts';
+import { Config } from './config/index.ts';
 
-login('admin');
+const startServer = () => {
+    const PORT = Config.PORT;
+    try {
+        app.listen(PORT, () => {
+            // eslint-disable-next-line no-console
+            console.log(`Server is running on port ${PORT}`);
+        });
+    } catch (error) {
+        // eslint-disable-next-line no-console
+        console.error('Error starting server:', error);
+        process.exit(1);
+    }
+};
+
+startServer();
